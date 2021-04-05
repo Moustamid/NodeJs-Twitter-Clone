b2be556f1fe7e0652c47b4b4fe13dc516da82573
Moustamid <karim1992moustamid@gmail.com>
2021-04-05 15:57:30 -0500

    apllication done

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # NodeJs-Twitter-Clone 👩‍💻
 
-# Testing :  🙋‍♂️
+- Tweets
+- Likes, comments and retweets
+- Profile pages
+- Following and followers support
+- Instant messaging in real time
+- Group chat support
+- Profile pictures
+- Cover photos
+
+# Testing : 🙋‍♂️
+
 - email : jhon@test.com
 - password : 123456
---
+++ database.js
@@ -0,0 +1,32 @@
+//The require(‘mongoose’) call above returns a Singleton object.
+//It means that the first time you call require(‘mongoose’), it
+//is creating an instance of the Mongoose class and returning it.
+//On subsequent calls, it will return the same instance that was
+//created and returned to you the first time because of how module
+//import/export works in ES6.
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    mongoose
+      .connect(
+        'mongodb+srv://moustamid:B5uf1g8p29@cluster0.xw9ci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      )
+      .then(() => {
+        console.log('database connection successful');
+      })
+      .catch((err) => {
+        console.log('database connection error ' + err);
+      });
+  }
+}
+
+module.exports = new Database();
